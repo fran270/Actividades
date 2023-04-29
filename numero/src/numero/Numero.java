f4abcb2aad617b18b0a4a131014b7b07cadd604a
fran270 <114153171+fran270@users.noreply.github.com>
2023-04-29 17:07:29 +0200

    Add files via upload

---
+++ numero/src/numero/Numero.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package numero;
+import java.util.Scanner;
+/**
+ *
+ * @author Fran Crespo Crespo
+ */
+public class Numero {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        
+        int numero;
+        
+        System.out.println("Introduce un numero");
+        numero = s.nextInt();
+        
+       
+    }
+    
+}
